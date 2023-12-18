6233ec9a8acf135a3ecb0842883c215b3ea90ee1
Antonio Gomes Castelo <oletsaca@gmail.com>
2023-12-18 15:43:11 +0100

    updated

--- macros/alter_table_set_rowfilter.sql
+++ macros/alter_table_set_rowfilter.sql
@@ -3,7 +3,12 @@
 
   {% if relation.is_table %}
     {% if source.filter %}
+      {% do log("ACA01") %}}
+      
       {% for k, v in source.filter.items() %}
+        {% do log("ACA02") %}}
+        {% do log({{k}}) %}}
+        {% do log({{v}}) %}}
         {% set sql %} alter table {{source.database}}.{{source.schema}}.{{source.name}} set row filter {{k}} on ({{v}}); {% endset %}
 
         {% do run_query(sql) %}
