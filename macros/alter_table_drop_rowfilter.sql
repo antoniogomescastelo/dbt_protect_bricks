2024379c8c5d1abbd4a7aabc2d9c10c6f5300b1a
Antonio Gomes Castelo <oletsaca@gmail.com>
2023-12-18 16:30:13 +0100

    updated

--- macros/alter_table_drop_rowfilter.sql
+++ macros/alter_table_drop_rowfilter.sql
@@ -2,9 +2,16 @@
   {% set relation = adapter.get_relation(database=source.database, schema=source.schema, identifier=source.name) %}
 
   {% if relation.is_table %}
+    {% set sql %} select distinct filter_name from {{source.database}}.INFORMATION_SCHEMA.COLUMN_TAGS where table_catalog = '{{source.database}}' and table_schema = '{{source.schema}}' and table_name = '{{source.name}}'; {% endset %}
+
+    {% set results = run_query(sql) %}
+
+    {% if results.columns[0].values() %}
+
       {% set sql %} alter table {{source.database}}.{{source.schema}}.{{source.name}} drop row filter; {% endset %}
 
       {% do run_query(sql) %}
       
+    {% endif %}
   {% endif %}
 {% endmacro %}
